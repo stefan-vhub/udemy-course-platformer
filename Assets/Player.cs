cf23cd7da55b450ae7bdb21ff42c088db1211cb1
Petrescu Viorel Stefan <64736176+stefan-vhub@users.noreply.github.com>
2024-12-19 21:17:39 +0200

    init

---
+++ Assets/Player.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), rb.velocity.y);
+    }
+}
