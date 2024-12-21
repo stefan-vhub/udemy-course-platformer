08a4b13fbb1ce0e4a804bb3306acb839c8d29938
Petrescu Viorel Stefan <64736176+stefan-vhub@users.noreply.github.com>
2024-12-21 11:19:12 +0200

    Jump

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -10,16 +10,25 @@ public class Player : MonoBehaviour
     private Rigidbody2D rb;
     private Animator anim;
 
-    [Header("Movement")]
+    [Header("Movement details")]
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
+    [SerializeField] private float doubleJumpForce;
+
 
     [Header("Collision Info")]
     [SerializeField] private float groundCheckDistance;
+    [SerializeField] private float wallCheckDistance;
     [SerializeField] private LayerMask whatIsGround;
     private bool isGrounded;
+    private bool isAirborne;
+    private bool isWallDetected;
 
+    public bool canDoubleJump;
     private float xInput;
+    private float yInput;
+    private bool facingRight = true;
+    private int facingDir = 1;
 
     private void Awake()
     {
@@ -29,45 +38,96 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        HandleCollision();
+        UpdateAirbornStatus();
+
         HandleInput();
+        HandleWallSlide();
         HandleMovement();
+        HandleFlip();
+        HandleCollision();
         HandleAnimations();
+    }
+
+    private void HandleWallSlide()
+    {
+        bool canWallSlide = isWallDetected && rb.velocity.y < 0;
+        float yModifer = yInput < 0 ? 1 : .05f;
+        if (!canWallSlide) return;
+        rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * yModifer);
+    }
+
+    private void UpdateAirbornStatus()
+    {
+        if (isAirborne && isGrounded) HandleLanding();
+        if (!isAirborne && !isGrounded) BecomeAirborne();
+    }
+
+    private void BecomeAirborne()
+    {
+        isAirborne = true;
+    }
+
+    private void HandleLanding()
+    {
+        isAirborne = false;
+        canDoubleJump = true;
     }
 
     private void HandleInput()
     {
         xInput = Input.GetAxisRaw("Horizontal");
+        yInput = Input.GetAxisRaw("Vertical");
 
-
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            Jump();
-        }
-    }
-
-    private void Jump()
-    {
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        if (Input.GetKeyDown(KeyCode.Space)) JumpButton();
     }
 
     private void HandleCollision()
     {
         isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+        isWallDetected = Physics2D.Raycast(transform.position, Vector2.right * facingDir, wallCheckDistance, whatIsGround);
     }
 
     private void HandleAnimations()
     {
         anim.SetFloat("xVelocity", rb.velocity.x);
+        anim.SetFloat("yVelocity", rb.velocity.y);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isWallDetected", isWallDetected);
     }
 
     private void HandleMovement()
     {
+        if (isWallDetected) return;
         rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+    }
+    private void HandleFlip()
+    {
+        if (xInput < 0 && facingRight || xInput > 0 && !facingRight) Flip();
+    }
+
+    private void Jump() => rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    private void DoubleJump()
+    {
+        canDoubleJump = false;
+        rb.velocity = new Vector2(rb.velocity.x, doubleJumpForce);
+    }
+
+    private void JumpButton()
+    {
+        if (isGrounded) Jump();
+        else if (canDoubleJump) DoubleJump();
+    }
+
+    private void Flip()
+    {
+        facingDir = facingDir * -1;
+        transform.Rotate(0, 180, 0);
+        facingRight = !facingRight;
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - groundCheckDistance));
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x + (wallCheckDistance * facingDir), transform.position.y));
     }
 }
